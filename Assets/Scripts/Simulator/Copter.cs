1cf0656e1df1aa330ca6a0005e2103ac430ba60d
Thomas Buck <xythobuz@xythobuz.de>
2017-04-02 01:44:42 +0200

    Also constrain and reset position of quad in simulator.
    Former-commit-id: e84bacb645cb917af5646543254012271fe37e86

--- Assets/Scripts/Simulator/Copter.cs
+++ Assets/Scripts/Simulator/Copter.cs
@@ -4,13 +4,16 @@ using SanAndreasUnity.Behaviours;
 namespace SanAndreasUnity.Simulator {
 	public class Copter : MonoBehaviour {
 		private bool isEnabled = false;
+		GameObject quad;
 
 		void Update () {
-			if ((!isEnabled) && Loader.HasLoaded) {
+			if (!Loader.HasLoaded) return;
+			
+			if (!isEnabled) {
 				isEnabled = true;
 
 				// Place copter above world and enable gravity (so it doesn't fall while loading)
-				GameObject quad = GameObject.Find ("QuadCopter");
+				quad = GameObject.Find ("QuadCopter");
 				Vector2 p = quad.transform.position;
 				p.y = 40.0f;
 				quad.transform.position = p;
@@ -19,6 +22,35 @@ namespace SanAndreasUnity.Simulator {
 				foreach (Rigidbody rb in rbs) {
 					rb.useGravity = true;
 				}
+			}
+
+			// Reset to a valid (and solid!) start position when falling below the world
+			if (quad.transform.position.y < -300) {
+				Transform spawn = GameObject.Find ("Player Spawns").GetComponentsInChildren<Transform> () [1];
+				quad.transform.position = spawn.position;
+				quad.transform.rotation = spawn.rotation;
+			}
+
+			// Constrain to stay inside map
+			if (quad.transform.position.x < -3000) {
+				var t = quad.transform.position;
+				t.x = -3000;
+				quad.transform.position = t;
+			}
+			if (quad.transform.position.x > 3000) {
+				var t = quad.transform.position;
+				t.x = 3000;
+				quad.transform.position = t;
+			}
+			if (quad.transform.position.z < -3000) {
+				var t = quad.transform.position;
+				t.z = -3000;
+				quad.transform.position = t;
+			}
+			if (quad.transform.position.z > 3000) {
+				var t = quad.transform.position;
+				t.z = 3000;
+				quad.transform.position = t;
 			}
 		}
 	}
