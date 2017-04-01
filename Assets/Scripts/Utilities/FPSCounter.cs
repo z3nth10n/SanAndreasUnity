8aa47dbaefc83ab6983bd52b02fddba5b7189185
Thomas Buck <xythobuz@xythobuz.de>
2017-04-01 20:05:18 +0200

    Created GUIGraph class for drawing graphs, taken from FPSCounter
    Former-commit-id: ca02622636120febc67af6c4c77a26485e146a26

--- Assets/Scripts/Utilities/FPSCounter.cs
+++ Assets/Scripts/Utilities/FPSCounter.cs
@@ -7,14 +7,17 @@ namespace SanAndreasUnity.Utilities {
 		private static int fpsTextureHeight = 25;
 		private static float fpsMaximum = 60.0f;
 		private static float fpsGreen = 50.0f;
-		private static float fpsRed = 23.0f;
+		private static float fpsYellow = 23.0f;
 		private float fpsDeltaTime = 0.0f;
-		private Texture2D fpsTexture = null;
 		private float[] fpsHistory = new float[fpsTextureWidth];
 		private int fpsIndex = 0;
+		private GUIGraph fpsGraph;
 
 		void Start () {
-			fpsTexture = new Texture2D (fpsTextureWidth, fpsTextureHeight, TextureFormat.RGBA32, false, true);
+			//fpsTexture = new Texture2D (fpsTextureWidth, fpsTextureHeight, TextureFormat.RGBA32, false, true);
+			fpsGraph = new GUIGraph(fpsTextureWidth, fpsTextureHeight, new Color (0.0f, 0.0f, 0.0f, 0.66f), new Color (1.0f, 0.0f, 0.0f, 1.0f), fpsMaximum);
+			fpsGraph.addLimit (fpsGreen, new Color (0.0f, 1.0f, 0.0f, 1.0f));
+			fpsGraph.addLimit (fpsYellow, new Color (1.0f, 1.0f, 0.0f, 1.0f));
 		}
 
 		void Update () {
@@ -28,43 +31,21 @@ namespace SanAndreasUnity.Utilities {
 			// Show FPS counter
 			float msec = fpsDeltaTime * 1000.0f;
 			float fps = 1.0f / fpsDeltaTime;
-			GUILayout.BeginArea (new Rect (15 + fpsTexture.width, Screen.height - 25, 100, 25));
+			GUILayout.BeginArea (new Rect (15 + fpsTextureWidth, Screen.height - 25, 100, 25));
 			GUILayout.Label (string.Format("{0:0.}fps ({1:0.0}ms)", fps, msec));
 			GUILayout.EndArea ();
 
-			if (fpsTexture == null) return;
-
-			// Show FPS history
-			Color[] colors = new Color[fpsTexture.width * fpsTexture.height];
-			Color cRed = new Color (1.0f, 0.0f, 0.0f, 1.0f);
-			Color cYellow = new Color (1.0f, 1.0f, 0.0f, 1.0f);
-			Color cGreen = new Color (0.0f, 1.0f, 0.0f, 1.0f);
-			for (int i = 0; i < (fpsTexture.width * fpsTexture.height); i++) {
-				colors [i] = new Color (0.0f, 0.0f, 0.0f, 0.66f); // Half-transparent background for FPS graph
-			}
-			fpsTexture.SetPixels (colors);
 			// Append to history storage
 			fpsHistory [fpsIndex] = fps;
-			int f = fpsIndex;
-			// Draw graph into texture
-			for (int i = fpsTexture.width - 1; i >= 0; i--) {
-				float graphVal = (fpsHistory [f] > fpsMaximum) ? fpsMaximum : fpsHistory [f];
-				int height = (int)(graphVal * fpsTexture.height / (fpsMaximum + 0.1f));
-				Color c = (fpsHistory[f] >= fpsGreen) ? cGreen : ((fpsHistory[f] <= fpsRed) ? cRed : cYellow);
-				fpsTexture.SetPixel(i, height, c);
-				f--;
-				if (f < 0) {
-					f = fpsHistory.Length - 1;
-				}
-			}
+
+			// Show FPS history
+			fpsGraph.draw(new Vector2(5, Screen.height - fpsTextureHeight - 5), fpsHistory, fpsIndex);
+
 			// Next entry in rolling history buffer
 			fpsIndex++;
 			if (fpsIndex >= fpsHistory.Length) {
 				fpsIndex = 0;
 			}
-			// Draw texture on GUI
-			fpsTexture.Apply (false, false);
-			GUI.DrawTexture (new Rect(5, Screen.height - fpsTexture.height - 5, fpsTexture.width, fpsTexture.height), fpsTexture);
 		}
 	}
 }
---
+++ Assets/Scripts/Utilities/GUIGraph.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+namespace SanAndreasUnity.Utilities {
+	public class GUIGraph {
+		private struct Limit {
+			public float v;
+			public Color c;
+
+			public Limit(float _v, Color _c) {
+				v = _v;
+				c = _c;
+			}
+		}
+
+		private Texture2D texture;
+		private Color background;
+		private Color foreground;
+		private System.Collections.Generic.List<Limit> limits;
+		private float maximum;
+
+		public GUIGraph (int width, int height, Color bg, Color fg, float max = float.NaN) {
+			texture = new Texture2D (width, height, TextureFormat.RGBA32, false, true);
+			background = bg;
+			foreground = fg;
+			maximum = max;
+		}
+
+		public void addLimit(float minValue, Color color) {
+			if (limits == null) {
+				limits = new System.Collections.Generic.List<Limit> ();
+			}
+
+			limits.Add (new Limit (minValue, color));
+			limits.Sort((x, y) => x.v < y.v ? -1 : (x.v == y.v ? 0 : 1));
+		}
+
+		public void draw (Vector2 pos, float[] values, int pointer) {
+			// Fill texture with background color
+			Color[] colors = new Color[texture.width * texture.height];
+			for (int i = 0; i < (texture.width * texture.height); i++) {
+				colors [i] = background;
+			}
+			texture.SetPixels (colors);
+
+			// Draw graph into texture
+			for (int i = texture.width - 1; i >= 0; i--) {
+				float graphVal = (values [pointer] > maximum) ? maximum : values [pointer];
+				int height = (int)(graphVal * texture.height / (maximum + 0.1f));
+				Color c = foreground;
+				if (limits != null) {
+					foreach (Limit l in limits) {
+						if (values [pointer] > l.v) {
+							c = l.c;
+						}
+					}
+				}
+				texture.SetPixel(i, height, c);
+				pointer--;
+				if (pointer < 0) {
+					pointer = values.Length - 1;
+				}
+			}
+
+			// Draw texture on GUI
+			texture.Apply (false, false);
+			GUI.DrawTexture (new Rect(pos, new Vector2(texture.width, texture.height)), texture);
+		}
+	}
+}
